f67c9fe0c273957701f71888f809a5691b1c8308
Erny Salcedo <ernydavid04@gmail.com>
2024-11-13 12:17:11 -0500

    changing to npm

--- components/ui/dashboard/user-info.tsx
+++ components/ui/dashboard/user-info.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/auth'
+import { Edit } from 'lucide-react'
 
 export async function UserInfoCard () {
   const session = await auth()
@@ -6,18 +7,24 @@ export async function UserInfoCard () {
 
   if (!user) {
     return (
-      <div>
-        <p>Ooppss... User not found!</p>
+      <div className=' grid gap-6'>
+        <div className='w-12 h-12 bg-muted animate-pulse'>
+          <div className='w-full h-9 bg-muted animate-pulse' />
+        </div>
       </div>
     )
   }
 
   return (
-    <div className='grid gap-6 place-content-start'>
-      <div className='flex flex-col items-start'>
-        <p className='text-3xl'>{user?.name}</p>
-        <p>{user?.email}</p>
-        <span className='primary-button flex justify-center items-center text-center px-2'>Precursor Regular</span>
+    <div className='w-full md:max-w-[260px] grid gap-6 place-content-start'>
+      <div className='flex flex-col items-start space-y-1'>
+        <p className='text-3xl tracking-tighter font-medium text-wrap leading-none'>{user?.name} Macea</p>
+        <div className='flex gap-3'>
+          <span className='primary-button flex justify-center items-center px-2 py-1 leading-tight'>"Y las cosas anteriores no seran recordadas ni subiran al corazón". Isaias 65: 17</span>
+          <button className='flex items-center justify-center secondary-button'>
+            <Edit className='w-4 h-4' />
+          </button>
+        </div>
       </div>
       <div className='flex flex-col items-start'>
         <p>Periodo de servicio:</p>
